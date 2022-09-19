8c3ff83ec00fa8097aa8a44d5d913d2b875c20d1
Anna Drumond <annadrumond@student.dei.uc.pt>
2022-09-19 18:19:54 +0100

    add projeto spring

---
+++ crud-spring/src/main/java/com/example/crudspring/CrudSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
+
+}
